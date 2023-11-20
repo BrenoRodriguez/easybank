26cee6321b2bdf5d0559229bac0f97e770d291e2
João Vitor Damasceno Muniz <95295666+joaomunizz7@users.noreply.github.com>
2023-11-20 20:51:16 -0300

    Add files via upload

---
+++ easy_bank.php
@@ -0,0 +1,9 @@
+<?php
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'easy_bank';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
+
